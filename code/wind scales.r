99d29f9afa96779fa9b0880054806f1de980c46c
Thomas Buehrens <tbuehrens@gmail.com>
2025-05-29 14:51:31 -0700

    split ages into maiden and repeat

--- code/wind scales.r
+++ code/wind scales.r
@@ -1,6 +1,3 @@
-
-
-
 pacman::p_load(DBI,tidyverse)
 #function
 select_table_PostgreSQL<-function(database_args,SQL){
@@ -42,11 +39,13 @@ age_lut<-select_table_PostgreSQL(database_args=database_args,SQL=SQL3)%>%
 
 dat<-read_csv(here::here("data/Wind_Steelhead_Scales.csv"))%>%
   filter(run==2)%>%
+  mutate(salt_age=gsub("s","S",salt_age))%>%
   mutate(age_code=paste0(fresh_age,".",salt_age))%>%
   right_join(age_lut,by=join_by(age_code))%>%
   filter(!is.na(sy_total_age) & !is.na(spawn_year))%>%
-  group_by(spawn_year,sy_total_age)%>%
+  mutate(maturity=ifelse(grepl("S",salt_age),"Repeat","Maiden"))%>%
+  group_by(spawn_year,maturity,sy_total_age)%>%
   summarize(n=n())%>%
   pivot_wider(names_from = sy_total_age,values_from = n,values_fill = 0)%>%
-  select(spawn_year, order(as.numeric(names(.)[-1])) + 1)%>%
+  arrange(maturity,spawn_year)%>%
   write_csv(here::here("results/Wind_Summer_Steelhead_Age_Comp.csv"))
